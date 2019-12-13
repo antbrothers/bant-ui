2211c62793f0d1642dc8b033fbc97aac23baae8f
antbrother <jianxi_lin@163.com>
2019-12-13 15:46:57 +0800

    修改细节

--- .storybook/addons.ts
+++ .storybook/addons.ts
@@ -1,8 +1,9 @@
 /*
  * @Author: linjianxi
  * @Date: 2019-12-12 16:42:00
- * @LastEditTime: 2019-12-12 17:57:53
+ * @LastEditTime: 2019-12-13 15:23:50
  * @Description: file content
  */
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
+// import '@storybook/addon-viewport/register';
--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,17 +1,19 @@
 /*
  * @Author: linjianxi
  * @Date: 2019-12-12 16:42:41
- * @LastEditTime: 2019-12-12 17:28:45
+ * @LastEditTime: 2019-12-13 15:21:17
  * @Description: file content
  */
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
 import "./storystyles.css"
 import "../node_modules/tachyons/css/tachyons.css"
 
 const req = require.context('../stories', true, /.stories.tsx$/);
-
+addParameters({
+  viewport: { defaultViewport: 'iphone6' }
+})
 function loadStories() {
   req.keys().forEach(req);
 }
