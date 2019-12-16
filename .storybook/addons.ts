6d5d696477f6e4d0ddeae87bb3eeced2b2cf073f
antbrother <jianxi_lin@163.com>
2019-12-16 12:03:54 +0800

    添加toolbar模块

--- .storybook/addons.ts
+++ .storybook/addons.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: linjianxi
  * @Date: 2019-12-12 16:42:00
- * @LastEditTime: 2019-12-13 15:23:50
+ * @LastEditTime: 2019-12-16 11:58:38
  * @Description: file content
  */
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
-// import '@storybook/addon-viewport/register';
+import '@storybook/addon-viewport/register';
